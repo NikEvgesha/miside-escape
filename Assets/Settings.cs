8ae789630581ba0fc39ddf210a1114c4b1c33574
Melomaner <>
2025-02-04 23:28:32 +0300

    добавил сетингс

--- Assets/Settings.cs
+++ Assets/Settings.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class Settings : MonoBehaviour
@@ -10,15 +9,44 @@ public class Settings : MonoBehaviour
      Language
      
      */
-    // Start is called before the first frame update
-    void Start()
+
+    // TODO: вынести в лобби, настройки должны быть всегда, настроить UI
+
+    public static Settings instance;
+    public Action<float> ChangeMouseSensitivity;
+    public Action<float> ChangeVolume;
+    [SerializeField] private GameObject _UIWindow;
+
+    private bool _isOpen;
+
+
+    private void OnEnable()
     {
-        
+        _isOpen = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    // отдельно музыка и звуки - только ползунки
+    private Settings()
     {
-        
+        instance = this;
+    }
+    public void Sensitivity(float sens)
+    {
+        ChangeMouseSensitivity?.Invoke(sens);
+    }
+
+    public void SoundVolume(float volume)
+    {
+        SoundManager.Instance.SoundVolume = volume;
+    }
+    public void MusicVolume(float volume)
+    {
+        SoundManager.Instance.MusicVolume = volume;
+    }
+
+    public void ToggleUIOpen()
+    {
+        _isOpen = !_isOpen;
+        _UIWindow.SetActive(_isOpen);
     }
 }
