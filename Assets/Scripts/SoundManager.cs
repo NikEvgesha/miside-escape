29cfb0e83a18eef9eb464842ca0b6f7f58f2b327
Melomaner <>
2025-02-04 22:23:12 +0300

    Добавил префабы

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,68 @@
+using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundManager : MonoBehaviour
+{
+    private const float minVolume = -80f;
+    private const float maxVolume = 0f;
+
+    [SerializeField] private AudioMixerGroup _mixer;
+    [SerializeField] private AnimationCurve _curve;
+    [SerializeField] private AudioSource _music;
+
+    private static SoundManager _instance;
+    private bool _soundON;
+    private float _soundVolume;
+    private float _musicVolume;
+    private string _soundName = "SoundVolume";
+    private string _musicName = "MusicVolume";
+
+
+    public static SoundManager Instance { get { return _instance; } }
+    public bool isSoundOn { get { return _soundON; } }
+    public float SoundVolume
+    {
+        get { return _soundVolume; }
+        set
+        {
+            _soundVolume = value;
+            _mixer.audioMixer.SetFloat(_soundName, Mathf.Lerp(minVolume, maxVolume, _curve.Evaluate(_soundVolume)));
+            //VolumeChange?.Invoke(_volume);
+        }
+    }
+    public float MusicVolume
+    {
+        get { return _musicVolume; }
+        set
+        {
+            _musicVolume = value;
+            _mixer.audioMixer.SetFloat(_musicName, Mathf.Lerp(minVolume, maxVolume, _curve.Evaluate(_musicVolume)));
+            //VolumeChange?.Invoke(_volume);
+        }
+    }
+
+
+    public Action<float> VolumeChange;
+
+    // volume change
+
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void Start()
+    {
+        _music.Play();
+    }
+
+}
