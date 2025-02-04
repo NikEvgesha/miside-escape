3ae94acb108c2b3dd0b142ff81cc8c078594d191
NikEvgesha <nikitenkowa.e@yandex.ru>
2025-02-04 21:22:54 +0300

    imports + base scene

---
+++ Assets/SaveManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveManager : MonoBehaviour
+{
+    /*
+     Топ-5 личных рекордов времени
+     Топ-5 по всем игрокам + место игрока
+     Топ месяца
+     */
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
